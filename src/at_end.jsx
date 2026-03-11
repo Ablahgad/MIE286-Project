2125c5d8795e66747be708f475c356d121fa11df
Ablah <115513366+Ablahgad@users.noreply.github.com>
2026-03-11 12:49:45 -0400

    Added built-in numbers to code

--- src/at_end.jsx
+++ src/at_end.jsx
@@ -3,7 +3,19 @@ import { useState, useEffect, useCallback } from 'react'
 function AtEnd() {
     const [numbers, setNumbers] = useState(Array(10).fill(""));
     const [num_style, setNum_style] = useState(Array(10).fill({}));
-    const ans = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    // const ans = ["8", "1", "3", "4", "6", "5", "1", "0", "4", "5"] // Ans2
+    const ans = ["5", "4", "0", "2", "7", "4", "5", "3", "1", "7"] // Ans3
+    // const ans = ["0", "2", "5", "9", "1", "7", "8", "8", "9", "6"] // Ans4
+    // const ans = ["5", "9", "0", "8", "8", "7", "8", "8", "1", "8"] // Ans5
+    // const ans = ["9", "5", "9", "5", "9", "7", "3", "5", "1", "3"] // Ans6
+    // const ans = ["1", "0", "4", "4", "8", "6", "6", "1", "9", "8"] // Ans7
+    // const ans = ["4", "7", "2", "9", "9", "3", "8", "3", "5", "7"] // Ans8
+    // const ans = ["3", "2", "2", "8", "5", "2", "3", "2", "3", "4"] // Ans9
+    // const ans = ["7", "2", "0", "8", "7", "1", "7", "9", "7", "0"] // Ans10
+    // const ans = ["9", "1", "8", "5", "5", "1", "7", "0", "7", "6"] // Ans11
+    // const ans = ["2", "7", "9", "3", "1", "5", "2", "3", "6", "1"] // Ans12
+    // const ans = ["4", "4", "5", "2", "7", "2", "0", "3", "4", "3"] // Ans13
+
 
     const handleKeyPress = useCallback((event) => {
         const key = event.key;
